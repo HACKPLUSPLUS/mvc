02ac82046e7ff79a4b170bf06d581ec577ff007a
HACKPLUSPLUS <jesse.wilson.dev@gmail.com>
2017-06-06 13:39:10 +0200

    Add minifier

--- controller/indexController.php
+++ controller/indexController.php
@@ -5,6 +5,30 @@ Class indexController Extends baseController
 
 	public function index()
 	{
+            // setup the URL, the CSS and the form data
+    $url = 'https://cssminifier.com/raw';
+    $css = file_get_contents('./public/css/template.css');
+    
+    $data = array(
+        'input' => $css,
+    );
+
+    // init the request, set some info, send it and finally close it
+    $ch = curl_init($url);
+
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $minified = curl_exec($ch);
+
+    var_dump($data);
+    curl_close($ch);
+
+    // output the $minified
+    echo $minified;
+    die;
 		
 		$veyron = AutomobileFactory::create( 'Bugatti', 'Veyron' );
 		var_dump( $veyron );
