8cc91c0c00c91ea91ca6d2322097dcddd494818d
HACKPLUSPLUS <jesse.wilson.dev@gmail.com>
2017-07-03 15:05:39 +0200

    join

---
+++ data/queries/join hut_codes_hutnummers.sql
@@ -0,0 +1,12 @@
+SELECT * FROM data_hut_codes
+WHERE rederij_id = 25 and schip_id = 146;
+/*SELECT * FROM data_schepen WHERE id = 146;*/
+SELECT * FROM data_hut_nummers
+LEFT JOIN data_hut_codes ON data_hut_codes.code = data_hut_nummers.categorie_code
+WHERE data_hut_nummers.rederij_id = 25
+AND data_hut_nummers.schip_id = 146
+AND data_hut_nummers.categorie_code = 'VF'
+AND data_hut_nummers.hutnummer = 8002;
+SELECT DISTINCT(data_hut_nummers.hutnummer) FROM data_hut_codes
+INNER JOIN data_hut_nummers ON data_hut_nummers.categorie_code = data_hut_codes.code
+WHERE data_hut_codes.code = 'VF';
