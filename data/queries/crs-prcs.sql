96ae4a1d04d93e39f0ea4808feb7263131afab72
HACKPLUSPLUS <jesse.wilson.dev@gmail.com>
2017-07-04 10:11:27 +0200

    wham

--- data/queries/crs-prcs.sql
+++ data/queries/crs-prcs.sql
@@ -1,3 +1,8 @@
 SELECT * FROM data_prijzen;
 SELECT * FROM data_prijzen
-INNER JOIN data_cruises ON data_cruises.id = data_prijzen.cruise_id;
+INNER JOIN data_cruises ON data_cruises.id = data_prijzen.cruise_id
+INNER JOIN data_hut_codes ON data_hut_codes.id = data_prijzen.hut_id
+WHERE data_cruises.begindatum > '2017-08-01'
+AND data_cruises.rederij_id = '25'
+AND data_cruises.id = 151561
+AND data_prijzen.prijs = '1198,00';
