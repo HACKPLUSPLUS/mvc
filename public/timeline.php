c879ff1d316bf64dc5295409afc1d96eedf0bf30
HACKPLUSPLUS <jesse.wilson.dev@gmail.com>
2017-08-02 16:25:12 +0200

    WFTTTTTTTTTTT

--- public/timeline.php
+++ public/timeline.php
@@ -2179,6 +2179,10 @@ from
       No saved replies yet.
     </div>
   </div>
+  <div class="blankslate">
+      <h3 class="mb-2">No OAuth applications</h3>
+      <p class="">OAuth applications are used to access the GitHub API. <a href="https://developer.github.com">Read the docs</a> to find out more.</p>
+      <a href="/settings/applications/new" class="btn btn-primary" data-pjax="true">Register a new application</a>    </div>
 </div>
 
 
