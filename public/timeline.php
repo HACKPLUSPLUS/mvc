b6b2fbc7eaba7ca691feeb7711122fea802a7389
HACKPLUSPLUS <jesse.wilson.dev@gmail.com>
2017-08-02 16:22:46 +0200

    WFTTTTTTTTTT

--- public/timeline.php
+++ public/timeline.php
@@ -2143,6 +2143,42 @@ from
     </div>
 <?php             include 'html_repository_content.php'; ?>
 <?php include 'html_pricing_setup.php'; ?>
+<div class="settings-next">
+    <div class="Subhead">
+      <h2 class="Subhead-heading">Saved replies</h2>
+    </div>
+  </div>
+<p>
+    Saved replies are re-usable text snippets that you can use throughout GitHub comment fields.
+    Saved replies can save you time if you’re often typing similar responses.
+    <a href="https://help.github.com/articles/working-with-saved-replies/">Learn more</a>.
+  </p>
+  <div class="saved-reply-settings-container js-saved-reply-container has-replies">
+    <div class="listgroup">
+        <div class="listgroup-item listgroup-item-preview js-saved-reply-list-item">
+          <div class="BtnGroup float-right">
+            <!-- '"` --><!-- </textarea></xmp> --></option></form><form accept-charset="UTF-8" action="/settings/replies/31377/edit" class="BtnGroup-form" method="get"><div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="&#x2713;" /></div>
+              <button type="submit" class="btn btn-sm BtnGroup-item" aria-label="Edit reply">
+                <svg aria-hidden="true" class="octicon octicon-pencil" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path fill-rule="evenodd" d="M0 12v3h3l8-8-3-3-8 8zm3 2H1v-2h1v1h1v1zm10.3-9.3L12 6 9 3l1.3-1.3a.996.996 0 0 1 1.41 0l1.59 1.59c.39.39.39 1.02 0 1.41z"/></svg>
+              </button>
+</form>            <!-- '"` --><!-- </textarea></xmp> --></option></form><form accept-charset="UTF-8" action="/settings/replies/31377" class="js-saved-reply-delete BtnGroup-form" data-remote="true" method="post"><div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="&#x2713;" /><input name="_method" type="hidden" value="delete" /><input name="authenticity_token" type="hidden" value="4l5mhJP9GQe+gFSPRDRPtyHZimZ1I2/f4V0q6yv4YihxkX2Q9y8VraYEwRgt3pzQ320UNYhpt8XV4hkKpr7AVQ==" /></div>
+              <button type="submit" class="btn btn-sm BtnGroup-item btn-danger" aria-label="Delete reply">
+                <svg aria-hidden="true" class="octicon octicon-x" height="16" version="1.1" viewBox="0 0 12 16" width="12"><path fill-rule="evenodd" d="M7.48 8l3.75 3.75-1.48 1.48L6 9.48l-3.75 3.75-1.48-1.48L4.52 8 .77 4.25l1.48-1.48L6 6.52l3.75-3.75 1.48 1.48z"/></svg>
+              </button>
+</form>          </div>
+          <span class="listgroup-item-title css-truncate">
+            <span class="css-truncate-target">Baby</span>
+          </span>
+          <span class="listgroup-item-body css-truncate">
+            <span class="css-truncate-target">Hello</span>
+          </span>
+        </div>
+    </div>
+
+    <div class="blankslate text-gray">
+      No saved replies yet.
+    </div>
+  </div>
 </div>
 
 
