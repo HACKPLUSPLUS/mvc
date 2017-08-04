7e6cf97b698b6762821cd365a2a908196304089f
HACKPLUSPLUS <jesse.wilson.dev@gmail.com>
2017-08-04 16:11:03 +0200

    ashes

--- public/html_discussion_sidebar.php
+++ public/html_discussion_sidebar.php
@@ -19,36 +19,8 @@
 
 
 
-  <div class="discussion-sidebar-item lock-toggle">
-    <a href="#lock-confirmation" rel="facebox" class="lock-toggle-link">
-        <svg aria-hidden="true" class="octicon octicon-lock" height="16" version="1.1" viewBox="0 0 12 16" width="12"><path fill-rule="evenodd" d="M4 13H3v-1h1v1zm8-6v7c0 .55-.45 1-1 1H1c-.55 0-1-.45-1-1V7c0-.55.45-1 1-1h1V4c0-2.2 1.8-4 4-4s4 1.8 4 4v2h1c.55 0 1 .45 1 1zM3.8 6h4.41V4c0-1.22-.98-2.2-2.2-2.2-1.22 0-2.2.98-2.2 2.2v2H3.8zM11 7H2v7h9V7zM4 8H3v1h1V8zm0 2H3v1h1v-1z"/></svg>
-      <strong>Lock conversation</strong>
-    </a>
 
-    <div id="lock-confirmation" style="display:none">
-      <h2 class="facebox-header" data-facebox-id="facebox-header">Lock conversation on this pull request</h2>
-
-      <p data-facebox-id="facebox-description">Locking the conversation means:</p>
-
-      <ul>
-          <li>Other users <strong>can’t add new comments</strong> to this pull request.</li>
-            <li>
-              You and other members of teams with
-              <a href="https://help.github.com/articles/what-are-the-different-access-permissions">write access</a>
-              to this repository <strong>can still leave comments</strong> that others can see.
-            </li>
-      </ul>
-
-      <p>You can always unlock this pull request again in the future.</p>
-
-      <div class="facebox-footer">
-        <!-- '"` --><!-- </textarea></xmp> --></option></form><form accept-charset="UTF-8" action="/cruisereizen/cruisereizen/issues/979/lock" method="post"><div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="&#x2713;" /><input name="_method" type="hidden" value="put" /><input name="authenticity_token" type="hidden" value="24nIV6M5qBXK2Q9sPv5jrp76sBqj2V2+XwnycT2IszMPRgbYNmsuxCN3+xNDKZZH9F+7/QxuOCwpphmTZWmjXw==" /></div>
-          <button type="submit" class="btn btn-danger btn-block">
-            Lock conversation on this pull request
-          </button>
-</form>      </div>
-    </div>
-  </div>
+      <?php include 'html_sidebar_lock.php'; ?>
 
 
 </div>
