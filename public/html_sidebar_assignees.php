7dd6b83d02a8dcd68f2d7de3c57d801cbb75e661
HACKPLUSPLUS <jesse.wilson.dev@gmail.com>
2017-08-04 10:29:45 +0200

    cut lmthtttt

---
+++ public/html_sidebar_assignees.php
@@ -0,0 +1,90 @@
+<div class="discussion-sidebar-item sidebar-assignee js-discussion-sidebar-item">
+  <!-- '"` --><!-- </textarea></xmp> --></option></form><form accept-charset="UTF-8" action="/cruisereizen/cruisereizen/issues/979?partial=issues%2Fsidebar%2Fshow%2Fassignees" class="js-issue-sidebar-form" data-remote="true" method="post"><div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="&#x2713;" /><input name="_method" type="hidden" value="put" /><input name="authenticity_token" type="hidden" value="Kz92R3EbRXshQTPqhV8OLU56ScrpbxWWZpDaHFGDa6f/9WYyjEwtuHvh8DczzfmbE9IzazHviwwIEn/7zIyBhQ==" /></div>
+
+
+  <div class="select-menu js-menu-container js-select-menu js-load-contents "
+       data-contents-url="/cruisereizen/cruisereizen/issues/979/show_partial?partial=issues%2Fsidebar%2Fassignees_menu_content"
+       data-multiple data-max-options=10>
+
+    <button type="button"
+            class="discussion-sidebar-heading discussion-sidebar-toggle js-menu-target"
+            aria-label="Assign up to 10 people to this issue" aria-haspopup="true" aria-expanded="false"
+            data-hotkey="a">
+      <svg aria-hidden="true" class="octicon octicon-gear" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path fill-rule="evenodd" d="M14 8.77v-1.6l-1.94-.64-.45-1.09.88-1.84-1.13-1.13-1.81.91-1.09-.45-.69-1.92h-1.6l-.63 1.94-1.11.45-1.84-.88-1.13 1.13.91 1.81-.45 1.09L0 7.23v1.59l1.94.64.45 1.09-.88 1.84 1.13 1.13 1.81-.91 1.09.45.69 1.92h1.59l.63-1.94 1.11-.45 1.84.88 1.13-1.13-.92-1.81.47-1.09L14 8.75v.02zM7 11c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3z"/></svg>
+      Assignees
+    </button>
+
+    <div class="select-menu-modal-holder js-menu-content js-navigation-container">
+      <div class="select-menu-modal">
+        <div class="select-menu-header">
+          <svg aria-label="Close" class="octicon octicon-x js-menu-close" height="16" role="img" version="1.1" viewBox="0 0 12 16" width="12"><path fill-rule="evenodd" d="M7.48 8l3.75 3.75-1.48 1.48L6 9.48l-3.75 3.75-1.48-1.48L4.52 8 .77 4.25l1.48-1.48L6 6.52l3.75-3.75 1.48 1.48z"/></svg>
+          <span class="select-menu-title">Assign up to 10 people to this issue</span>
+        </div>
+
+
+
+<div class="select-menu-filters">
+  <div class="select-menu-text-filter">
+    <input type="text" id="assignee-filter-field" class="form-control js-filterable-field js-navigation-enable"
+           placeholder="Filter people" aria-label="Type or choose a name" spellcheck="false" autocomplete="off">
+  </div>
+</div>
+
+<div class="js-max-warning warning d-none">
+  You can only select 10 assignees.
+</div>
+
+<div class="select-menu-list">
+    <button class="btn-block select-menu-clear-item select-menu-item js-navigation-item"
+         type="button" aria-label="Clear assignee" data-clear-assignees>
+      <svg aria-hidden="true" class="octicon octicon-x select-menu-item-icon" height="16" version="1.1" viewBox="0 0 12 16" width="12"><path fill-rule="evenodd" d="M7.48 8l3.75 3.75-1.48 1.48L6 9.48l-3.75 3.75-1.48-1.48L4.52 8 .77 4.25l1.48-1.48L6 6.52l3.75-3.75 1.48 1.48z"/></svg>
+      <div class="select-menu-item-text">Clear assignees</div>
+    </button>
+
+  <div class="select-menu-no-results">Nothing to show</div>
+
+  <input type="hidden" value="" name="issue[user_assignee_ids][]">
+
+  <div data-filterable-for="assignee-filter-field" data-filterable-type="substring-memory" data-filterable-limit="100" role="menu">
+      <input type="hidden" name="issue[user_assignee_ids][]" value="15982332">
+
+    <template>
+      <div class="select-menu-item js-navigation-item" role="menuitem">
+        <svg aria-hidden="true" class="octicon octicon-check select-menu-item-icon" height="16" version="1.1" viewBox="0 0 12 16" width="12"><path fill-rule="evenodd" d="M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5z"/></svg>
+
+        <input style="display:none"
+               type="checkbox"
+               value=""
+               name="issue[user_assignee_ids][]">
+
+        <div class="select-menu-item-gravatar">
+          <img src="" alt="" size="20" class="avatar avatar-small mr-1 js-avatar">
+        </div>
+
+        <div class="select-menu-item-text lh-condensed">
+          <span class="select-menu-item-heading">
+            <span class="js-username"></span>
+            <span class="description js-description"></span>
+          </span>
+        </div>
+      </div>
+    </template>
+  </div>
+</div>
+
+
+          <div class="select-menu-loading-overlay anim-pulse">
+            <svg aria-hidden="true" class="octicon octicon-octoface" height="32" version="1.1" viewBox="0 0 16 16" width="32"><path fill-rule="evenodd" d="M14.7 5.34c.13-.32.55-1.59-.13-3.31 0 0-1.05-.33-3.44 1.3-1-.28-2.07-.32-3.13-.32s-2.13.04-3.13.32c-2.39-1.64-3.44-1.3-3.44-1.3-.68 1.72-.26 2.99-.13 3.31C.49 6.21 0 7.33 0 8.69 0 13.84 3.33 15 7.98 15S16 13.84 16 8.69c0-1.36-.49-2.48-1.3-3.35zM8 14.02c-3.3 0-5.98-.15-5.98-3.35 0-.76.38-1.48 1.02-2.07 1.07-.98 2.9-.46 4.96-.46 2.07 0 3.88-.52 4.96.46.65.59 1.02 1.3 1.02 2.07 0 3.19-2.68 3.35-5.98 3.35zM5.49 9.01c-.66 0-1.2.8-1.2 1.78s.54 1.79 1.2 1.79c.66 0 1.2-.8 1.2-1.79s-.54-1.78-1.2-1.78zm5.02 0c-.66 0-1.2.79-1.2 1.78s.54 1.79 1.2 1.79c.66 0 1.2-.8 1.2-1.79s-.53-1.78-1.2-1.78z"/></svg>
+          </div>
+      </div>
+    </div>
+  </div>
+
+    <span class="css-truncate">
+      <p>
+        <img alt="@jariketting" class="avatar" height="20" src="https://avatars1.githubusercontent.com/u/15982332?v=4&amp;s=40" width="20" />
+        <a href="/cruisereizen/cruisereizen/pulls/assigned/jariketting" class="assignee css-truncate-target">jariketting</a>
+      </p>
+</span>
+
+</form></div>
